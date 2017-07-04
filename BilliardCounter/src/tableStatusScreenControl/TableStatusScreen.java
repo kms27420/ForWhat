0872c3fc1048a6f707812c8fac1eb9c3e3112e9e
Kwon <Kwon@59.7.203.189>
2017-07-04 12:30:22 +0900

    Screen에는 없는 변수 추가 및 컴포넌트들 추가
    Screen에는 없는 변수 SCREEN_ROW, SCREEN_COL를 추가하여 본 Screen의 위치 정보를 자기 자신이 갖고
    있게끔 하였다.
    생성자를 통해 위치 정보와 스크린 번호 정보를 전달 받는다.
    추상 매서드 구현을 ScreenList가 없어짐에 맞춰서 새롭게 구현하였다.

--- BilliardCounter/src/tableStatusScreenControl/TableStatusScreen.java
+++ BilliardCounter/src/tableStatusScreenControl/TableStatusScreen.java
@@ -1,10 +1,16 @@
 package tableStatusScreenControl;
 
 import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.Font;
 
-import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
+import javax.swing.text.JTextComponent;
+
+import component.ImageIconFactory;
+import component.Screen;
+import gameSetting.GameSettingWindowOpenButton;
 
 
 /**
@@ -14,45 +20,109 @@ import javax.swing.border.BevelBorder;
  * @author Kwon
  *
  */
-public class TableStatusScreen extends JPanel{
-	private final TableStatusScreenContents TABLE_STATUS_SCREEN_CONTENTS = new TableStatusScreenContents();
+public class TableStatusScreen extends Screen{
+	private final JTextComponent SCREEN_NUMBER_TEXTFIELD = new JTextField();	// 테이블 번호를 보여주는 TextField
+	private final JTextComponent USED_TIME_TEXTFIELD = new JTextField();		// 사용 시간을 보여주는 TextField
+	private final JTextComponent TOTAL_FEE_TEXTFIELD = new JTextField();		// 현재 요금을 보여주는 TextField
+	private final GameSettingWindowOpenButton GAME_SETTING_SCREEN_OPEN_BUTTON = new GameSettingWindowOpenButton(this);	// GameSettingWindow을 open해주는 버튼
+	private final int SCREEN_ROW, SCREEN_COL;
 	
-	private boolean isInitedAlready;
+	protected boolean isBeingPlayed;
 	
-	public TableStatusScreen(){
-		this.initTableStatusScreen();
+	public TableStatusScreen(int row, int col, int screenNumber){
+		this.setScreenNumber(screenNumber);
+		SCREEN_ROW = row;
+		SCREEN_COL = col;
+		
+		this.initThisScreen();
 		this.addComponents();
 	}
-	private void initTableStatusScreen(){
+	public boolean getIsBeingPlayed(){
+		return isBeingPlayed;
+	}
+	@Override
+	protected void initThisScreen() {
 		this.setLayout(null);
 		this.setBackground(new Color(0x00DD00));
 		this.setBorder(new BevelBorder(BevelBorder.RAISED));
 	}
-	private void addComponents(){
-		this.add(TABLE_STATUS_SCREEN_CONTENTS.getUsedTimeTextField());
-		this.add(TABLE_STATUS_SCREEN_CONTENTS.getTotalFeeTextField());
-		this.add(TABLE_STATUS_SCREEN_CONTENTS.getGameSettingScreenOpenButton());
-	}
-	private void initComponents(int width, int height){
-		TABLE_STATUS_SCREEN_CONTENTS.initComponents(width, height);
-	}
-	/**
-	 * TableStatusScreen 인스턴스의 TABLE_STATUS_SCREEN_CONTENTS를 받아오는 매서드
-	 * @return TABLE_STATUS_SCREEN_CONTENTS
-	 */
-	public TableStatusScreenContents getTableStatusScreenContents(){
-		return TABLE_STATUS_SCREEN_CONTENTS;
-	}
-	
 	@Override
-	public void paint(Graphics g){
-		super.paint(g);
-		if(!isInitedAlready){
-			isInitedAlready = true;
-			this.initComponents(this.getWidth(), this.getHeight());
-			this.repaint();
-			this.revalidate();
+	protected void initComponents() {
+		int width = this.getWidth() / 2;
+		int height = this.getHeight() / 4;
+		int x = width / 4;
+		int y = height;
+		
+		Font font = new Font("MD아트체", Font.BOLD, 25);
+		Color color = new Color(0x00DD00);
+		
+		SCREEN_NUMBER_TEXTFIELD.setBounds(x, y - height, width, height);
+		SCREEN_NUMBER_TEXTFIELD.setFont(font);
+		SCREEN_NUMBER_TEXTFIELD.setText(this.getScreenNumber() + "번 테이블");
+		SCREEN_NUMBER_TEXTFIELD.setEditable(false);
+		SCREEN_NUMBER_TEXTFIELD.setBackground(color);
+		SCREEN_NUMBER_TEXTFIELD.setBorder(null);
+		
+		USED_TIME_TEXTFIELD.setBounds(x, y, width, height);		
+		USED_TIME_TEXTFIELD.setFont(font);
+		USED_TIME_TEXTFIELD.setEditable(false);
+		USED_TIME_TEXTFIELD.setBackground(color);
+		USED_TIME_TEXTFIELD.setBorder(null);
+		
+		TOTAL_FEE_TEXTFIELD.setBounds(x, y + height, width, height);
+		TOTAL_FEE_TEXTFIELD.setFont(font);
+		TOTAL_FEE_TEXTFIELD.setEditable(false);
+		TOTAL_FEE_TEXTFIELD.setBackground(color);
+		TOTAL_FEE_TEXTFIELD.setBorder(null);
+		
+		width = width / 3 * 2;
+		height = height / 3 * 4;
+		x = width * 2;
+		y = height * 2;
+		
+		GAME_SETTING_SCREEN_OPEN_BUTTON.setBounds(x, y, width, height);
+		
+	}
+	@Override
+	protected void addComponents() {
+		this.add(SCREEN_NUMBER_TEXTFIELD);
+		this.add(USED_TIME_TEXTFIELD);
+		this.add(TOTAL_FEE_TEXTFIELD);
+		this.add(GAME_SETTING_SCREEN_OPEN_BUTTON);
+	}
+	@Override
+	protected void updateComponents() {
+		ImageIcon imageIcon;
+		
+		if(!isBeingPlayed){
+			isBeingPlayed = true;
+			imageIcon = ImageIconFactory.makeImageIcon("src\\resource\\", "PayTableImage.png");
+			GAME_SETTING_SCREEN_OPEN_BUTTON.setIcon(imageIcon);
+			
+			Thread thread = new Thread(new Thread(){
+				private final String TIME_MESSAGE = "사용 시간 : ";
+				private final String FEE_MESSAGE = "요금 : ";
+				private final int FEE_PER_MIN = 120;
+				private int usedMin;
+				@Override
+				public void run(){
+					while(GAME_SETTING_SCREEN_OPEN_BUTTON.getIcon().equals(imageIcon)){
+						try{
+							USED_TIME_TEXTFIELD.setText(TIME_MESSAGE + usedMin / 60 + " : " + usedMin % 60);
+							TOTAL_FEE_TEXTFIELD.setText(FEE_MESSAGE + usedMin * FEE_PER_MIN);
+							sleep(1000);
+							usedMin++;
+						}catch(InterruptedException e){e.printStackTrace();}
+					}
+				}
+			});
+			thread.start();
+		}else{
+			isBeingPlayed = false;
+			imageIcon = ImageIconFactory.makeImageIcon("src\\resource\\", "UseTableImage.png");
+			GAME_SETTING_SCREEN_OPEN_BUTTON.setIcon(imageIcon);
+			USED_TIME_TEXTFIELD.setText("");
+			TOTAL_FEE_TEXTFIELD.setText("");
 		}
 	}
-	
 }

dac42d540a6735d1c232837490b55e34f8d191c8
Kwon <Kwon@59.7.203.189>
2017-07-04 12:24:32 +0900

    수정된 Board클래스에 맞춰 추상 매서드의 기능들을 구현

--- BilliardCounter/src/tableStatusScreenControl/TableStatusBoard.java
+++ BilliardCounter/src/tableStatusScreenControl/TableStatusBoard.java
@@ -1,8 +1,8 @@
 package tableStatusScreenControl;
 
-import java.awt.Graphics;
-
-import javax.swing.JPanel;
+import component.Board;
+import component.BoardAligner;
+import tableStatusScreenCreate.SettingWindowOpenScreen;
 
 /**
  * 
@@ -11,58 +11,66 @@ import javax.swing.JPanel;
  * @author Kwon
  *
  */
-public class TableStatusBoard extends JPanel{
-	private final TableStatusScreenCreateButton TABLE_STATUS_SCREEN_CREATE_BUTTON = new TableStatusScreenCreateButton(this);	// TableStatusScreen 생성 버튼
-	private final TableStatusScreenAligner TABLE_STATUS_SCREEN_ALIGNER = new TableStatusScreenAligner(this);	// TableStatusScreen 정렬자
-	
-	private static int createButtonWidth, createButtonHeight;
-	private boolean isInitedAlready;
-	
+public class TableStatusBoard extends Board{
 	public TableStatusBoard(){
-		this.initTableStatusBoard();
+		super(4,4);
+		this.initThisBoard();
+		this.initScreens();
+		this.initBoardAligner(new TableStatusBoardAligner());
 		this.addComponents();
 	}
-	private void initTableStatusBoard() {
+
+	@Override
+	protected void initThisBoard() {
 		this.setLayout(null);
-		TableStatusScreenList.initTableStatusScreenList();				// TableStatusScreenList를 초기화 해줌
-	}
-	private void addComponents(){
-		this.add(TABLE_STATUS_SCREEN_CREATE_BUTTON);
-	}
-	private void arrangeScreenCreateButton(){
-		int x = TableStatusScreenList.getNextCol() * createButtonWidth;
-		int y = TableStatusScreenList.getNextRow() * createButtonHeight;
-		TABLE_STATUS_SCREEN_CREATE_BUTTON.setBounds(x, y, createButtonWidth, createButtonHeight);
-	}
-	private void initButtonSize(){
-		createButtonWidth = this.getWidth() / TableStatusScreenList.getMaxCol();
-		createButtonHeight = this.getHeight() / 2;
-	}
-	/**
-	 * TableStatusScreen을 TableStatusBoard에 그리는 작업을 하는 매서드
-	 */
-	public void paintTableStatusScreen(){
-		if(TableStatusScreenList.createTableStatusScreen()){	// TableStatusScreen이 성공적으로 증설 되었다면
-			TABLE_STATUS_SCREEN_ALIGNER.arrangeNewScreen();		// 증설된 Screen을 정렬해줌
-			this.arrangeScreenCreateButton();
-			this.repaint();
-			this.revalidate();
-		}else	System.out.println("더 이상 당구대를 증설할 수 없습니다.");
 	}
 	@Override
-	public void paint(Graphics g){
-		super.paint(g);
-		
-		if(!isInitedAlready){
-			isInitedAlready = true;
-			this.initButtonSize();
-			
-			TABLE_STATUS_SCREEN_ALIGNER.arrangeLoadedScreens();
-			this.arrangeScreenCreateButton();
-			
-			this.repaint();
-			this.revalidate();
-			
+	protected void initScreens(){
+		for(int i = 0; i < this.getMaxRow(); i++){
+			for(int j = 0; j < this.getMaxCol(); j++){
+				SCREEN[i][j] = new SettingWindowOpenScreen(i, j, this);
+			}
 		}
+	}
+	@Override
+	protected void initBoardAligner(BoardAligner boardAligner) {
+		this.boardAligner = boardAligner;
+	}
+	@Override
+	protected void addComponents() {
+		for(int i = 0; i < this.getMaxRow(); i++){
+			for(int j = 0; j < this.getMaxCol(); j++){
+				if(SCREEN[i][j] != null)
+					this.add(SCREEN[i][j]);
+			}
+		}
+	}
+	
+	@Override
+	public boolean createScreen(int row, int col, int screenNumber) {
+		if(IS_NUMBER_USED_ALREADY[screenNumber])	return false;
+		
+		IS_NUMBER_USED_ALREADY[screenNumber] = true;
+		
+		this.remove(SCREEN[row][col]);
+		
+		int x = SCREEN[row][col].getX();
+		int y = SCREEN[row][col].getY();
+		int width = SCREEN[row][col].getWidth();
+		int height = SCREEN[row][col].getHeight();
+		
+		SCREEN[row][col] = new TableStatusScreen(row, col, screenNumber);
+		SCREEN[row][col].setBounds(x, y, width, height);
+		this.add(SCREEN[row][col]);
+		
+		this.repaint();
+		this.revalidate();
+		
+		return true;
+	}
+
+	@Override
+	protected boolean deleteScreen(int row, int col) {
+		return false;
 	}
 }

ec140085c1792bac502f7d11029e52333d909390
Kwon <Kwon@DESKTOP-CPH25AC>
2017-08-29 13:27:17 +0900

    Add execution class
    본 프로그램을 시작하는 시작 클래스를 추가하였다.

---
+++ BilliardCounter/src/com/kms/billiardcounter/execution/Execution.java
@@ -0,0 +1,19 @@
+package com.kms.billiardcounter.execution; 
+
+import com.kms.billiardcounter.billiardcounterframe.BilliardCounterFrame;
+
+/**
+ * 
+ * 실행 클래스, main함수가 있다.
+ * 
+ * @author Kwon
+ *
+ */
+public class Execution {
+	
+	public static void main(String[] args) {
+		
+		new BilliardCounterFrame();
+		
+	}
+}

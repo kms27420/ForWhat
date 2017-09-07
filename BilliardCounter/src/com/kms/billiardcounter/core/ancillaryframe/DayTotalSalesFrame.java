2eccb0f8ad4cc018327995fe11f5ee8ca5caad88
Kwon <Kwon@DESKTOP-CPH25AC>
2017-09-07 13:11:10 +0900

    패키지 위치 변경
    원하는 날짜의 매출을 데이터베이스에 접근해서 구해주는 클래스로써 dao.daysales 패키지로 위치 변경

---
+++ BilliardCounter/src/com/kms/billiardcounter/core/ancillaryframe/DayTotalSalesFrame.java
@@ -0,0 +1,70 @@
+package com.kms.billiardcounter.core.ancillaryframe;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import com.kms.billiardcounter.dao.daysales.DaySalesLoader;
+import com.kms.billiardcounter.font.FontProvider;
+
+/**
+ * 
+ * 금일 매출을 확인하는 프레임
+ * 
+ * @author Kwon
+ *
+ */
+public class DayTotalSalesFrame extends JFrame {
+
+	public DayTotalSalesFrame() {
+		
+		initThisFrame();
+		
+		add( createDayTotalSalesPanel() );
+		
+	}
+	
+	private void initThisFrame() {
+		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		Dimension frameSize = new Dimension( 500, 400 );
+		
+		setLocation( screenSize.width / 2 - frameSize.width / 2, screenSize.height / 2 - frameSize.height / 2 );
+		setSize( frameSize );
+		
+		setLayout( new GridLayout( 1, 1 ) );
+		
+		setDefaultCloseOperation( DISPOSE_ON_CLOSE );
+		
+		setVisible( true );
+		
+	}
+	
+	private JPanel createDayTotalSalesPanel() {
+		
+		JPanel daySalesPanel = new JPanel();
+		
+		JLabel daySalesLabel = new JLabel();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat( "yyMMdd" );
+		
+		String date = sdf.format( new Date() );
+		
+		daySalesLabel.setText( "오늘 총 매출 : " + String.format( "%,10d" , DaySalesLoader.getDaySales( date ) ) + " 원"  );
+		daySalesLabel.setFont( FontProvider.getDefaultFont() );
+		daySalesLabel.setHorizontalAlignment( JLabel.CENTER );
+		
+		daySalesPanel.setLayout( new GridLayout( 1, 1 ) );
+		daySalesPanel.add( daySalesLabel );
+		
+		return daySalesPanel;
+		
+	}
+	
+}

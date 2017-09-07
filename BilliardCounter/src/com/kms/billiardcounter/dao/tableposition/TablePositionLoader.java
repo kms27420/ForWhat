e9be4a1c750964cdeb3b230b052227fa5b7bcf2b
Kwon <Kwon@DESKTOP-CPH25AC>
2017-09-07 13:03:22 +0900

    패키지 위치 변경
    데이터베이스를 처리하며 당구대들의 위치 정보를 다루는 클래스들로써 dao.tableposition 패키지로 위치 변경

---
+++ BilliardCounter/src/com/kms/billiardcounter/dao/tableposition/TablePositionLoader.java
@@ -0,0 +1,61 @@
+package com.kms.billiardcounter.dao.tableposition;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.kms.billiardcounter.dao.connection.BilliardCounterConnector;
+
+/**
+ * 
+ * 당구 테이블의 인터페이스 위치 정보를 load하는 클래스
+ * 
+ * @author Kwon
+ *
+ */
+
+public class TablePositionLoader {
+	
+	private TablePositionLoader(){}
+	
+	/**
+	 * 위치 정보를 통해 당구 테이블의 번호를 반환하는 매서드
+	 * @param tableRow 원하는 당구 테이블의 row
+	 * @param tableCol 원하는 당구 테이블의 col
+	 * @return tableNumber
+	 */
+	public static final int getTableNumberByRowAndCol( int tableRow, int tableCol ){
+		
+		int tableNumber = 0;
+		
+		try{
+			
+			String sql = "SELECT TABLE_NUMBER "
+					+ "FROM BILLIARD_COUNTER.CREATED_TABLE_INFO "
+					+ "WHERE TABLE_ROW = " + tableRow + " AND TABLE_COL = " + tableCol + ";";
+			
+			Connection conn = BilliardCounterConnector.getConnection();
+			Statement stmt = conn.createStatement();
+			
+			ResultSet rs = stmt.executeQuery( sql );
+			
+			while(rs.next()){
+				
+				tableNumber = rs.getInt("TABLE_NUMBER");
+				
+			}
+			
+			rs.close();
+			stmt.close();
+			conn.close();
+			
+		}catch(Exception e){
+		
+			//e.printStackTrace();
+		
+		}
+		
+		return tableNumber;
+	
+	}
+}
---
+++ BilliardCounter/src/com/kms/billiardcounter/dao/tableposition/TablePositionSaver.java
@@ -0,0 +1,58 @@
+package com.kms.billiardcounter.dao.tableposition;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import com.kms.billiardcounter.dao.connection.BilliardCounterConnector;
+
+/**
+ * 
+ * 당구 테이블의 인터페이스 위치 정보를 DB에 저장해주는 클래스
+ * 
+ * @author Kwon
+ *
+ */
+public class TablePositionSaver {
+	
+	private TablePositionSaver(){}
+	
+	private static final void createTableIfNotExists( Connection conn, Statement stmt ) throws Exception{
+		
+		stmt.execute(
+				"CREATE TABLE IF NOT EXISTS billiard_counter.CREATED_TABLE_INFO("
+				+ "TABLE_NUMBER INT(11) PRIMARY KEY NOT NULL,"
+				+ "TABLE_ROW INT(11) NOT NULL,"
+				+ "TABLE_COL INT(11) NOT NULL);"
+				);
+		
+	}
+	
+	public static final boolean saveTablePosition( int tableNumber, int row, int col ){
+		
+		try{
+			
+			Connection conn = BilliardCounterConnector.getConnection();
+			Statement stmt = conn.createStatement();
+			String sql;
+			
+			createTableIfNotExists( conn, stmt );
+			
+			sql = "INSERT INTO billiard_counter.CREATED_TABLE_INFO VALUES(" + tableNumber + ", " + row + ", " + col + ");";
+			stmt.executeUpdate(sql);
+			
+			stmt.close();
+			conn.close();
+			
+			return true;
+			
+		}catch(Exception e){
+			
+			//e.printStackTrace();
+			
+			return false;
+			
+		}
+		
+	}
+	
+}

d18eda44a576a173954ff79f65f7635a0a6754a7
Kwon <Kwon@DESKTOP-CPH25AC>
2017-09-07 13:02:26 +0900

    패키지 위치 변경
    본 프로그램에서 사용하는 정보를 다루는 클래스로써 support 패키지로 위치를 변경

---
+++ BilliardCounter/src/com/kms/billiardcounter/support/GameFeeInfo.java
@@ -0,0 +1,113 @@
+package com.kms.billiardcounter.support;
+
+/**
+ * 
+ * 게임 사용 정보를 저장하는 데이터 클래스
+ * 
+ * @author Kwon
+ *
+ */
+
+public class GameFeeInfo {
+	
+	private String date, startTime, endTime;
+	private int gameNumber, tableNumber, usedTime, fee;
+	private boolean isPaid;
+	
+	public String getDate(){
+		
+		return date;
+		
+	}
+	
+	public int getGameNumber(){
+		
+		return gameNumber;
+		
+	}
+	
+	public int getTableNumber(){
+		
+		return tableNumber;
+		
+	}
+	
+	public String getStartTime(){
+		
+		return startTime;
+		
+	}
+	
+	public String getEndTime(){
+		
+		return endTime;
+		
+	}
+	
+	public int getUsedTime(){
+		
+		return usedTime;
+		
+	}
+	
+	public int getFee(){
+		
+		return fee;
+		
+	}
+	
+	public boolean getIsPaid(){
+		
+		return isPaid;
+		
+	}
+	
+	public void setDate( String date ){
+		
+		this.date = date;
+		
+	}
+	
+	public void setGameNumber( int gameNumber ){
+		
+		this.gameNumber = gameNumber;
+		
+	}
+	
+	public void setTableNumber( int tableNumber ){
+		
+		this.tableNumber = tableNumber;
+		
+	}
+	
+	public void setStartTime( String startTime ){
+		
+		this.startTime = startTime;
+		
+	}
+	
+	public void setEndTime( String endTime ){
+		
+		this.endTime = endTime;
+		
+	}
+	
+	public void setUsedTime( int usedTime ){
+		
+		this.usedTime = usedTime;
+		
+	}
+	
+	public void setFee( int fee ){
+		
+		this.fee = fee;
+		
+	}
+	
+	public void setIsPaid( boolean isPaid ){
+		
+		this.isPaid = isPaid;
+		
+	}
+	
+}

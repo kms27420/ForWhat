6ce708f61aacf802821644aaa7834b9e58b8b8df
Kwon <Kwon@DESKTOP-CPH25AC>
2017-09-07 13:14:03 +0900

    패키지 위치 변경
    Contents들을 갖는 pane을 update시켜주는 인터페이스로써 core.contentspaneupdater 패키지로 위치
    변경

---
+++ BilliardCounter/src/com/kms/billiardcounter/core/contentspaneupdater/ContentsPaneUpdater.java
@@ -0,0 +1,19 @@
+package com.kms.billiardcounter.core.contentspaneupdater;
+
+/**
+ * 
+ * Content 들을 갖는 pane들이 화면 구성을 변경할 때 구현할 인터페이스
+ * 
+ * @author Kwon
+ *
+ */
+public interface ContentsPaneUpdater {
+	
+	/**
+	 * 
+	 * pane의 화면 구성을 변경해주는 매서드
+	 * 
+	 */
+	public void update();
+	
+}

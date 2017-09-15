d15e606979536be403f66ac6477b0d83c07a1f06
Kwon <Kwon@DESKTOP-CPH25AC>
2017-09-15 15:23:29 +0900

    클래스&인터페이스 추가
    계정 주인에게만 접근이 허용된 창에 접근할 시 계정 주인임을 확인한 후 창을 열어주는 클래스와 창을 여는 기능을 가진 인터페이스
    추가

---
+++ BilliardCounter/src/com/kms/billiardcounter/core/ancillaryframe/admin_approval/FrameOpener.java
@@ -0,0 +1,14 @@
+package com.kms.billiardcounter.core.ancillaryframe.admin_approval;
+
+/**
+ * 
+ * 새로운 Frame을 생성해주는 인터페이스
+ * 
+ * @author Kwon
+ *
+ */
+public interface FrameOpener {
+
+	public void openFrame();
+	
+}

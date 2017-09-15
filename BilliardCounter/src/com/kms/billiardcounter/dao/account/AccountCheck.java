11cc13c460ac818d987bb7bb67926b7c8fc30863
Kwon <Kwon@DESKTOP-CPH25AC>
2017-09-15 15:07:41 +0900

    새로운 매서드 추가, 클래스와 매서드 설명(주석) 추가
    패스워드를 판단하는 매서드를 추가하였고 본 클래스와 매서드의 설명을 작성한 주석을 추가하였다.

--- BilliardCounter/src/com/kms/billiardcounter/dao/account/AccountCheck.java
+++ BilliardCounter/src/com/kms/billiardcounter/dao/account/AccountCheck.java
@@ -6,10 +6,64 @@ import java.sql.Statement;
 
 import com.kms.billiardcounter.dao.connection.BilliardCounterConnector;
 
+/**
+ * 
+ * 본 프로그램의 계정(데이터베이스의 ACCOUNT 테이블에 접근하는)을 판별하는 클래스
+ * 
+ * @author Kwon
+ *
+ */
 public class AccountCheck {
 
 	private AccountCheck() {}
 	
+	/**
+	 * 
+	 * 입력 받은 password가 본 프로그램의 계정의 password와 일치하는지 판단해주는 매서드
+	 * 
+	 * @param password 입력받은 password
+	 * @return 일치하면 true, 그렇지 않으면 false
+	 */
+	public static final boolean isItCorrectPassword( String password ) {
+		
+		boolean returnValue = false;
+		
+		try {
+			
+			Connection conn = BilliardCounterConnector.getConnection();
+			Statement stmt = conn.createStatement();
+			String sql = "SELECT * "
+					+ "FROM billiard_counter.ACCOUNT "
+					+ "WHERE PASSWORD = '" + password + "';";
+			
+			ResultSet rs = stmt.executeQuery( sql );
+			
+			if( rs.next() )		returnValue = true;
+			
+			rs.close();
+			stmt.close();
+			conn.close();
+			
+		} catch( Exception e ) {
+			
+			e.printStackTrace();
+			
+			return false;
+			
+		}
+		
+		return returnValue;
+		
+	}
+	
+	/**
+	 * 
+	 * 입력받은 id, password가 본 프로그램의 계정 정보와 일치하는지 판단하는 매서드
+	 * 
+	 * @param id 입력받은 id
+	 * @param password 입력받은 password
+	 * @return 일치하면 true, 그렇지 않으면 false
+	 */
 	public static final boolean isItCorrectAccount( String id, String password ) {
 		
 		boolean returnValue = false;
@@ -42,6 +96,12 @@ public class AccountCheck {
 		
 	}
 	
+	/**
+	 * 
+	 * 본 프로그램의 계정이 생성되어있는지 판단하는 매서드
+	 * 
+	 * @return 계정이 존재하면 true, 그렇지 않으면 false
+	 */
 	public static final boolean isThereAccountInDB() {
 		
 		boolean returnValue = false;

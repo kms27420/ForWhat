d12b742bab222dce924b859b23f80ac821baf8a8
Kwon <Kwon@DESKTOP-CPH25AC>
2017-09-16 16:39:04 +0900

    불필요 매서드 삭제 & 기존 매서드 형태 수정
    본 panel 클래스에 불필요하게 다시 panel를 만들어 추가하는 createGameTableNumberViewerPanel를
    createGameTableNumberDisplayAndDeleteLabel 매서드로 변경하여 바로 label를 추가하게끔
    수정하였고 생성되어있던 당구 테이블을 삭제하는 기능을 추가하였다.

--- BilliardCounter/src/com/kms/billiardcounter/core/mainframe/components/bottom/components/components/top/BilliardTableNumberControlPanel.java
+++ BilliardCounter/src/com/kms/billiardcounter/core/mainframe/components/bottom/components/components/top/BilliardTableNumberControlPanel.java
@@ -1,12 +1,18 @@
 package com.kms.billiardcounter.core.mainframe.components.bottom.components.components.top;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -14,7 +20,8 @@ import javax.swing.border.BevelBorder;
 
 import com.kms.billiardcounter.core.contentspaneupdater.ContentsPaneUpdater;
 import com.kms.billiardcounter.core.string.NumericManufacturer;
-import com.kms.billiardcounter.dao.tableposition.TablePositionSaver;
+import com.kms.billiardcounter.dao.nonpaidgames.NonPaidGamesLoader;
+import com.kms.billiardcounter.dao.tableposition.TablePositionUpdater;
 import com.kms.billiardcounter.font.FontProvider;
 
 public class BilliardTableNumberControlPanel extends JPanel {
@@ -23,7 +30,15 @@ public class BilliardTableNumberControlPanel extends JPanel {
 		
 		initThisPanel();
 		
-		add( createThisContentsPanel( tableNumber, row, col, contentsPaneUpdater ) );
+		if( tableNumber == 0 ){
+			
+			add( createGameTableNumberCreatePanel( tableNumber, row, col, contentsPaneUpdater ) );
+			
+		} else{
+			
+			add( createGameTableNumberDisplayAndDeleteLabel( tableNumber, row, col, contentsPaneUpdater ) );
+			
+		}
 		
 		repaint();
 		revalidate();
@@ -37,38 +52,119 @@ public class BilliardTableNumberControlPanel extends JPanel {
 		
 	}
 	
-	private JPanel createThisContentsPanel( int tableNumber, int row, int col, ContentsPaneUpdater contentsPaneUpdater ){
-		
-		JPanel thisContentsPanel;
-		
-		if( tableNumber == 0 ){
-			
-			thisContentsPanel = createGameTableNumberCreatePanel( tableNumber, row, col, contentsPaneUpdater );
-			
-		} else{
-			
-			thisContentsPanel = createGameTableNumberViewerPanel( tableNumber );
-			
-		}
-		
-		return thisContentsPanel;
-		
-	}
-	
-	private JPanel createGameTableNumberViewerPanel( int tableNumber ) {
-		
-		JPanel gameTableNumberViewerPanel = new JPanel();
+	private JLabel createGameTableNumberDisplayAndDeleteLabel( int tableNumber, int row, int col, ContentsPaneUpdater contentsPaneUpdater ) {
 		
 		JLabel gameTableNumberLabel = new JLabel();			
 		
 		gameTableNumberLabel.setFont( FontProvider.getDefaultFont() );
 		gameTableNumberLabel.setHorizontalAlignment( JLabel.CENTER );
 		gameTableNumberLabel.setText( tableNumber + "번 테이블" );
+		gameTableNumberLabel.setOpaque( true );
+		gameTableNumberLabel.addMouseListener( new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				
+				e.getComponent().setBackground( null );
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				
+				e.getComponent().setBackground( Color.LIGHT_GRAY );
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				if( NonPaidGamesLoader.getNonPaidGameFeeInfoList( tableNumber ).size() > 0 )	return;
+				
+				Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+				Dimension frameSize = new Dimension( 400, 200 );
+				
+				JFrame tablePositionDeleteFrame = new JFrame( "당구대 빼기" );
+				
+				JPanel topPanel = new JPanel();
+				JLabel guidanceLabel = new JLabel( "정말 " + tableNumber + "번 당구대를 제거하시겠습니까?" );
+				
+				JPanel bottomPanel = new JPanel();
+				JButton confirmButton = new JButton( "제거" );
+				JButton cancelButton = new JButton( "취소" );
+				
+				guidanceLabel.setPreferredSize( new Dimension( 300, 50 ) );
+				guidanceLabel.setHorizontalTextPosition( JLabel.CENTER );
+				guidanceLabel.setFont( FontProvider.getDefaultFont() );
+				
+				topPanel.setBorder( BorderFactory.createEmptyBorder( 20, 0, 0, 0 ) );
+				topPanel.add( guidanceLabel );
+				
+				confirmButton.setPreferredSize( new Dimension( 80, 50 ) );
+				confirmButton.addActionListener( new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+
+						if( TablePositionUpdater.deleteTablePosition( tableNumber ) ) {
+							
+							BilliardTableNumberControlPanel.this.remove( gameTableNumberLabel );
+							BilliardTableNumberControlPanel.this.add( createGameTableNumberCreatePanel( tableNumber, row, col, contentsPaneUpdater ) );
+							
+							tablePositionDeleteFrame.dispose();
+							
+							contentsPaneUpdater.update();
+							
+						}
+						
+					}
+					
+				} );
+				
+				cancelButton.setPreferredSize( new Dimension( 80, 50 ) );
+				cancelButton.addActionListener( new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						
+						tablePositionDeleteFrame.dispose();
+						
+					}
+					
+				} );
+				
+				bottomPanel.setPreferredSize( new Dimension( 0, 60 ) );
+				bottomPanel.setBorder( BorderFactory.createEmptyBorder( 0, 0, 5, 0 ) );
+				bottomPanel.add( confirmButton );
+				bottomPanel.add( cancelButton );
+				
+				tablePositionDeleteFrame.setLocation( screenSize.width / 2 - frameSize.width / 2, screenSize.height / 2 - frameSize.height / 2 );
+				tablePositionDeleteFrame.setSize( frameSize );
+				tablePositionDeleteFrame.setLayout( new BorderLayout() );
+				tablePositionDeleteFrame.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+				
+				tablePositionDeleteFrame.add( topPanel, BorderLayout.CENTER );
+				tablePositionDeleteFrame.add( bottomPanel, BorderLayout.SOUTH );
+				
+				tablePositionDeleteFrame.setVisible( true );
+				
+			}
+			
+		} );
 		
-		gameTableNumberViewerPanel.setLayout( new GridLayout(1, 1) );
-		gameTableNumberViewerPanel.add( gameTableNumberLabel );
-		
-		return gameTableNumberViewerPanel;
+		return gameTableNumberLabel;
 		
 	}
 	
@@ -91,10 +187,10 @@ public class BilliardTableNumberControlPanel extends JPanel {
 				
 				int integerOfInputText = NumericManufacturer.getIntConsistingOnlyOfNumeric( gameTableNumberInputTextField.getText() );
 				
-				if( TablePositionSaver.saveTablePosition( integerOfInputText, row, col ) ){
+				if( TablePositionUpdater.saveTablePosition( integerOfInputText, row, col ) ){
 				
 					BilliardTableNumberControlPanel.this.remove( gameTableNumberCreatePanel );
-					BilliardTableNumberControlPanel.this.add( createGameTableNumberViewerPanel( integerOfInputText ) );
+					BilliardTableNumberControlPanel.this.add( createGameTableNumberDisplayAndDeleteLabel( integerOfInputText, row, col, contentsPaneUpdater ) );
 				
 					contentsPaneUpdater.update();
 				
